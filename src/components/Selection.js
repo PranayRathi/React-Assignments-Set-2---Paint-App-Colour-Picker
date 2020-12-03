2965fe35aefcb124993e0fa89bca2b7405c9e63b
PranayRathi <40464551+PranayRathi@users.noreply.github.com>
2020-12-03 19:41:48 +0530

    try1

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React from "react";
 
+export default function Selection(props) {
+  const [styleAttri, setStyleAttri] = React.useState({ background: "" });
+  return (
+    <>
+      <div
+        className="fix-box"
+        style={styleAttri}
+        onClick={() => props.applyColor(setStyleAttri)}
+      >
+        <h2 className="subheading">Selection</h2>
+      </div>
+    </>
+  );
+}
